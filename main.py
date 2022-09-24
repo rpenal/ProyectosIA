e08e0b05788a5a446c78181d50eb1b196f969daf
Raul Peña <rpenal@unal.edu.co>
2022-09-24 18:20:11 -0500

    BFS v1

--- main.py
+++ main.py
@@ -5,6 +5,127 @@ import numpy as np
 p.init()
 
 
+class BFSNode:
+    def __init__(self,position,directions,path,validPositions):
+        self.position = position
+        self.directions = directions
+        self.path = path
+        self.validPositions = validPositions
+
+
+
+
+class BFSPath:
+
+    def __init__(self,startPoint,objective):
+        self.start = tuple(np.subtract((Alto,Ancho),startPoint))
+        self.objective = objective
+
+        self.Paths = collections.deque()
+        valPos = validPositions.copy()
+        self.Paths.append(BFSNode(self.start,[],[self.start],valPos))
+
+    def tryUp(self,node):
+        nextMove = tuple(np.subtract(node.position,(verticalStep,0)))
+
+        validMove = nextMove in node.validPositions
+        if node.path != None:
+            Backtrack = nextMove in node.path
+
+
+        return validMove and not Backtrack
+
+    def tryDown(self,node):
+        nextMove = tuple(np.add(node.position,(verticalStep,0)))
+
+        validMove = nextMove in node.validPositions
+        if node.path != None:
+            Backtrack = nextMove in node.path
+
+        return validMove and not Backtrack
+
+    def tryLeft(self,node):
+        nextMove = tuple(np.subtract(node.position,(0,horizontalStep)))
+
+        validMove =  nextMove in node.validPositions
+        if node.path != None:
+            Backtrack = nextMove in node.path
+
+        return validMove and not Backtrack
+
+    def tryRight(self,node):
+        nextMove = tuple(np.add(node.position,(0,horizontalStep)))
+
+        validMove = nextMove in node.validPositions
+        if node.path != None:
+            Backtrack = nextMove in node.path
+
+        return validMove and not Backtrack
+
+    def CheckObjective(self,position):
+        return (position == self.objective)
+
+
+    def explore(self):
+        if self.Paths:
+            node = self.Paths.popleft()
+            if not self.CheckObjective(node.position):
+                if self.tryUp(node):
+                    newPosition = tuple(np.subtract(node.position,(verticalStep,0)))
+                    newValidPositions = node.validPositions
+                    if node.position in newValidPositions:
+                        newValidPositions.remove(node.position)
+                    newNode = BFSNode(newPosition,node.directions.copy(),node.path.copy(),newValidPositions)
+                    newNode.directions.append("up")
+                    newNode.path.append(newPosition)
+                    self.Paths.append(newNode)
+
+                if self.tryLeft(node):
+                    newPosition = tuple(np.subtract(node.position,(0,horizontalStep)))
+                    newValidPositions = node.validPositions
+                    if node.position in newValidPositions:
+                        newValidPositions.remove(node.position)
+                    newNode = BFSNode(newPosition,node.directions.copy(),node.path.copy(),newValidPositions)
+                    newNode.directions.append("left")
+                    newNode.path.append(newPosition)
+                    self.Paths.append(newNode)
+
+                if self.tryDown(node):
+                    newPosition = tuple(np.add(node.position,(verticalStep,0)))
+                    newValidPositions = node.validPositions
+                    if node.position in newValidPositions:
+                        newValidPositions.remove(node.position)
+                    newNode = BFSNode(newPosition,node.directions.copy(),node.path.copy(),newValidPositions)
+                    newNode.directions.append("down")
+                    newNode.path.append(newPosition)
+                    self.Paths.append(newNode)
+
+                if self.tryRight(node):
+                    newPosition = tuple(np.add(node.position,(0,horizontalStep)))
+                    newValidPositions = node.validPositions
+                    if node.position in newValidPositions:
+                        newValidPositions.remove(node.position)
+                    newNode = BFSNode(newPosition,node.directions.copy(),node.path.copy(),newValidPositions)
+                    newNode.directions.append("right")
+                    newNode.path.append(newPosition)
+                    self.Paths.append(newNode)
+
+                return 0
+
+            else:
+                print("Objective found")
+                print("Path:")
+
+                for action in node.directions:
+                    print(action)
+
+                return node
+
+
+        return -1
+
+
+
 class DFSPath:
 
     def __init__(self,start,objective):
@@ -79,20 +200,18 @@ class DFSPath:
 
             else:
                 if self.actualPath:
-                    print(validPositions)
-                    print(f"self.actualPath[-1] = {self.actualPath[-1]}")
                     validPositions.remove(self.actualPath.pop())
-                    print(validPositions)
                     self.actualPosition = self.actualPath[-1]
                 self.directions.pop()
 
 
-            print(self.directions)
         else:
             print("Objective found")
             print("Path:")
             for action in self.directions:
                 print(action)
+
+        return 1
 
 
 
@@ -156,21 +275,28 @@ mapa = rm.ConvertMatrixToMap(m)
 gameOver = False
 
 startPosition = (1 * (Alto/y),2 * (Ancho/y))
-objecitvePosition = (0 + (Alto/y), 0 + (Ancho/y))
+objecitvePosition = (0, 0 + (Ancho/y))
 
 horizontalStep = Ancho/y
 verticalStep = Alto/y
 
 
 
-DFSagent = DFSPath(startPosition,objecitvePosition)
+
 
 
 listaMuros = construir_mapa(mapa, y)
 validPositions = [(muro[1],muro[0]) for muro in listaMuros]
 listaPuntos = construir_puntos(mapa, y)
 
+DFSagent = DFSPath(startPosition,objecitvePosition)
+BFSagent = BFSPath(startPosition,objecitvePosition)
+
+state = 0
+
 while not gameOver:
+
+
 
     reloj.tick(15)
 
@@ -187,9 +313,21 @@ while not gameOver:
     p.draw.rect(ventana,(0,255,0),p.Rect(Ancho-startPosition[1] + (Ancho/(4*y)),Alto-startPosition[0] + (Alto/(4*y)),(Ancho/y)/2,(Alto/y)/2))
     p.draw.rect(ventana,(255,0,0),p.Rect(objecitvePosition[1] + (Ancho/(4*y)),objecitvePosition[0] + (Alto/(4*y)),(Ancho/y)/2,(Alto/y)/2))
 
-    DFSagent.explore()
-    print(DFSagent.actualPosition)
-    p.draw.rect(ventana,(255,255,0),p.Rect(DFSagent.actualPosition[1] + (Ancho/(4*y)),DFSagent.actualPosition[0] + (Alto/(4*y)),(Ancho/(2*y)),(Alto/(2*y))))
+    #DFSagent.explore()
+    #print(DFSagent.actualPosition)
+    #p.draw.rect(ventana,(255,255,0),p.Rect(DFSagent.actualPosition[1] + (Ancho/(4*y)),DFSagent.actualPosition[0] + (Alto/(4*y)),(Ancho/(2*y)),(Alto/(2*y))))
+
+
+    if state == 0:
+        state = BFSagent.explore()
+
+        for possiblePath in BFSagent.Paths:
+                p.draw.rect(ventana,(255,255,0),p.Rect(possiblePath.position[1] + (Ancho/(4*y)),possiblePath.position[0] + (Alto/(4*y)),(Ancho/(2*y)),(Alto/(2*y))))
+
+    if state not in [0,-1]:
+        for action in state.path:
+            p.draw.rect(ventana,(255,255,0),p.Rect(action[1] + (Ancho/(4*y)),action[0] + (Alto/(4*y)),(Ancho/(2*y)),(Alto/(2*y))))
+
 
     p.display.flip()
 
