207f0fd376aeb0c91a15c491f01bbc86e66096cd
Raul Peña <rpenal@unal.edu.co>
2022-09-24 01:21:06 -0500

    DFS algorithm

--- main.py
+++ main.py
@@ -1,6 +1,103 @@
 import pygame as p
 import Functions.Read_Maze as rm
+import collections
+import numpy as np
 p.init()
+
+
+class DFSPath:
+
+    def __init__(self,start,objective):
+        self.start = tuple(np.subtract((Alto,Ancho),start))
+        self.objective = objective
+
+        self.actualPosition = self.start
+
+        self.actualPath = collections.deque()
+        self.actualPath.append(self.actualPosition)
+
+        self.directions = collections.deque()
+
+
+    def tryUp(self):
+        nextMove = tuple(np.subtract(self.actualPosition,(verticalStep,0)))
+
+        validMove = nextMove in validPositions
+        Backtrack = nextMove in self.actualPath
+
+        return validMove and not Backtrack
+
+    def tryDown(self):
+        nextMove = tuple(np.add(self.actualPosition,(verticalStep,0)))
+
+        validMove = nextMove in validPositions
+        Backtrack = nextMove in self.actualPath
+
+        return validMove and not Backtrack
+
+    def tryLeft(self):
+        nextMove = tuple(np.subtract(self.actualPosition,(0,horizontalStep)))
+
+        validMove =  nextMove in validPositions
+        Backtrack = nextMove in self.actualPath
+
+        return validMove and not Backtrack
+
+    def tryRight(self):
+        nextMove = tuple(np.add(self.actualPosition,(0,horizontalStep)))
+
+        validMove = nextMove in validPositions
+        Backtrack = nextMove in self.actualPath
+
+        return validMove and not Backtrack
+
+    def CheckObjective(self):
+        return (self.actualPosition == self.objective)
+
+
+    def explore(self):
+        if not self.CheckObjective():
+            if self.tryUp():
+                self.actualPosition = tuple(np.subtract(self.actualPosition,(verticalStep,0)))
+                self.directions.append("up")
+                self.actualPath.append(self.actualPosition)
+
+            elif self.tryLeft():
+                self.actualPosition = tuple(np.subtract(self.actualPosition,(0,horizontalStep)))
+                self.directions.append("left")
+                self.actualPath.append(self.actualPosition)
+
+            elif self.tryDown():
+                self.actualPosition = tuple(np.add(self.actualPosition,(verticalStep,0)))
+                self.directions.append("down")
+                self.actualPath.append(self.actualPosition)
+
+            elif self.tryRight():
+                self.actualPosition = tuple(np.add(self.actualPosition,(0,horizontalStep)))
+                self.directions.append("right")
+                self.actualPath.append(self.actualPosition)
+
+            else:
+                if self.actualPath:
+                    print(validPositions)
+                    print(f"self.actualPath[-1] = {self.actualPath[-1]}")
+                    validPositions.remove(self.actualPath.pop())
+                    print(validPositions)
+                    self.actualPosition = self.actualPath[-1]
+                self.directions.pop()
+
+
+            print(self.directions)
+        else:
+            print("Objective found")
+            print("Path:")
+            for action in self.directions:
+                print(action)
+
+
+
+
+
 
 #-------------------Muros------------------------
 def construir_mapa(mapa, n):
@@ -29,6 +126,7 @@ def construir_puntos(mapa, n):
         y+=Alto/n
     return listaPuntos
 
+
 def dibujar_muro ( superficie , rectangulo, color ) : #Dibujamos un rectángulo
    p.draw.rect( superficie , color , rectangulo )
 
@@ -50,22 +148,34 @@ AZUL=(0,0,128)
 NEGRO=(0,0,0)
 BLANCO=(255,255,255)
 
-x=input("Name of the maze: ")
 y=int(input("size of maze: "))
 
-m= rm.ReadMaze(x)
+m= rm.ReadMaze(f"maze_{y}x{y}.csv")
 mapa = rm.ConvertMatrixToMap(m)
 
 gameOver = False
 
+startPosition = (1 * (Alto/y),2 * (Ancho/y))
+objecitvePosition = (0 + (Alto/y), 0 + (Ancho/y))
+
+horizontalStep = Ancho/y
+verticalStep = Alto/y
+
+
+
+DFSagent = DFSPath(startPosition,objecitvePosition)
+
+
+listaMuros = construir_mapa(mapa, y)
+validPositions = [(muro[1],muro[0]) for muro in listaMuros]
+listaPuntos = construir_puntos(mapa, y)
+
 while not gameOver:
 
-    reloj.tick(30)
-    Ancho = Pantalla.get_width()
-    Alto= Pantalla.get_height()
-    listaMuros = construir_mapa(mapa, y)
-    listaPuntos = construir_puntos(mapa, y)
-    
+    reloj.tick(15)
+
+
+
     for event in p.event.get():
         if event.type == p.QUIT:
             gameOver=True
@@ -74,7 +184,17 @@ while not gameOver:
     ventana.fill(BLANCO)
     #------------Dibujo------------------
     dibujar_mapa (ventana , listaMuros, listaPuntos)
+    p.draw.rect(ventana,(0,255,0),p.Rect(Ancho-startPosition[1] + (Ancho/(4*y)),Alto-startPosition[0] + (Alto/(4*y)),(Ancho/y)/2,(Alto/y)/2))
+    p.draw.rect(ventana,(255,0,0),p.Rect(objecitvePosition[1] + (Ancho/(4*y)),objecitvePosition[0] + (Alto/(4*y)),(Ancho/y)/2,(Alto/y)/2))
+
+    DFSagent.explore()
+    print(DFSagent.actualPosition)
+    p.draw.rect(ventana,(255,255,0),p.Rect(DFSagent.actualPosition[1] + (Ancho/(4*y)),DFSagent.actualPosition[0] + (Alto/(4*y)),(Ancho/(2*y)),(Alto/(2*y))))
+
     p.display.flip()
+
+
+
 
 p.quit()
 
